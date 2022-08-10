a2d426dca13578d9f8bafcd4b3ec47a9a9c6de5f
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2022-08-10 19:25:01 +0600

    Postgres data source configuration has been added

---
+++ data-jdbc/src/main/java/dev/spring/datajdbc/config/Config.java
@@ -0,0 +1,32 @@
+package dev.spring.datajdbc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Config {
+    @Value("${spring.datasource.url}")
+    private String URL;
+    @Value("${spring.datasource.username}")
+    private String USERNAME;
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+    @Value("${spring.datasource.driver-class-name}")
+    private String DRIVER;
+
+    @Bean
+    public DataSource postgresDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DRIVER);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+}

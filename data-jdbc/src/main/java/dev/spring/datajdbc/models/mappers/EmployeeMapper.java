95e44a900b271bdbf2a6fc27e387c698d0fc552f
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2022-08-10 23:43:33 +0600

    Employee mapper has been added

---
+++ data-jdbc/src/main/java/dev/spring/datajdbc/models/mappers/EmployeeMapper.java
@@ -0,0 +1,21 @@
+package dev.spring.datajdbc.models.mappers;
+
+import dev.spring.datajdbc.models.entities.Employee;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class EmployeeMapper implements RowMapper<Employee> {
+    @Override
+    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Employee employee = new Employee();
+        employee.setId(rs.getLong("id"));
+        employee.setFirstName(rs.getString("first_name"));
+        employee.setLastName(rs.getString("last_name"));
+        employee.setDepartmentId(rs.getLong("department_id"));
+        return employee;
+    }
+}

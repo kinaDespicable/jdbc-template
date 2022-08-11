f247ab814ed74757795e70cb14b7ef57bd629964
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2022-08-11 14:07:21 +0600

    DepartmentMapper has been created

---
+++ data-jdbc/src/main/java/dev/spring/datajdbc/models/mappers/DepartmentMapper.java
@@ -0,0 +1,18 @@
+package dev.spring.datajdbc.models.mappers;
+
+import dev.spring.datajdbc.models.entities.Department;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DepartmentMapper implements RowMapper<Department> {
+    @Override
+    public Department mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Department department = new Department();
+        department.setId(rs.getLong("id"));
+        department.setDepartmentName(rs.getString("department_name"));
+        department.setDepartmentCode(rs.getString("department_code"));
+        return department;
+    }
+}

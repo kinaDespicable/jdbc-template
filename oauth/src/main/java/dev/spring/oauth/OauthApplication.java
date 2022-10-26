adb7ba80226f7f0e581c586befc85cfff1c8330a
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2022-10-26 16:28:59 +0600

    Configuration properties has been enabled

--- oauth/src/main/java/dev/spring/oauth/OauthApplication.java
+++ oauth/src/main/java/dev/spring/oauth/OauthApplication.java
@@ -1,9 +1,12 @@
 package dev.spring.oauth;
 
+import dev.spring.oauth.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class OauthApplication {
 
 	public static void main(String[] args) {
